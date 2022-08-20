0008d20937f654b7d94445442ea95b1323029db8
SpearJin <ckdwls2525@gmail.com>
2022-08-20 20:18:50 +0900

    add: 화면 탐색

---
+++ src/screens/DetailScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const DetailScreen = () => {
+  return (
+    <View>
+      <Text>디테일 스크린</Text>
+    </View>
+  );
+};
+
+export default DetailScreen;
---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text>홈 스크린</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
